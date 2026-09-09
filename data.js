cf801f9f25a5c51f3303c66b7e3d5b59dda0cebc
fahmijasni <muhammadfahmi.jasni@gmail.com>
2026-09-09 16:54:35 +0800

    update 2

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bedroom-type-6",
-      "name": "Bedroom (type 6)",
+      "id": "0-type-6-view-1",
+      "name": "Type 6 View 1",
       "levels": [
         {
           "tileSize": 256,
@@ -33,14 +33,64 @@ var APP_DATA = {
           "yaw": 1.1674111075209037,
           "pitch": 0.20076390183176684,
           "rotation": 0,
-          "target": "1-bathroom-type-6"
+          "target": "2-type-6-bathroom-"
+        },
+        {
+          "yaw": 1.807536966696059,
+          "pitch": 0.2384312581072301,
+          "rotation": 0,
+          "target": "1-type-6-view-2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-bathroom-type-6",
-      "name": "Bathroom (type 6)",
+      "id": "1-type-6-view-2",
+      "name": "Type 6 View 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5620760009908743,
+          "pitch": 0.3479279308079093,
+          "rotation": 0,
+          "target": "2-type-6-bathroom-"
+        },
+        {
+          "yaw": 3.1097850605197888,
+          "pitch": 0.1621465145411669,
+          "rotation": 0,
+          "target": "0-type-6-view-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-type-6-bathroom-",
+      "name": "Type 6 Bathroom ",
       "levels": [
         {
           "tileSize": 256,
@@ -71,7 +121,13 @@ var APP_DATA = {
           "yaw": -1.8933631867301752,
           "pitch": 0.10634646227240552,
           "rotation": 0,
-          "target": "0-bedroom-type-6"
+          "target": "0-type-6-view-1"
+        },
+        {
+          "yaw": 3.101095733353427,
+          "pitch": 0.20269443598354364,
+          "rotation": 0,
+          "target": "1-type-6-view-2"
         }
       ],
       "infoHotspots": []
@@ -80,7 +136,7 @@ var APP_DATA = {
   "name": "Permas Hotel",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
+    "autorotateEnabled": false,
     "fullscreenButton": true,
     "viewControlButtons": true
   }
